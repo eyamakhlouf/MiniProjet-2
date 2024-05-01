edc416a9c368073fceccf51ca7f4fa05a4bd0f71
eyamakhlouf <153847341+eyamakhlouf@users.noreply.github.com>
2024-05-01 18:42:48 +0100

    MiniProjet2

---
+++ src/main/java/com/eya/livres/service/LivreService.java
@@ -0,0 +1,37 @@
+package com.eya.livres.service;
+
+import java.util.List;
+
+import org.springframework.data.domain.Page;
+
+import com.eya.livres.entities.Genre;
+import com.eya.livres.entities.Livre;
+
+public interface LivreService {
+	Livre saveLivre(Livre p);
+
+	Livre updateLivre(Livre p);
+
+	void deleteLivre(Livre p);
+
+	void deleteLivreById(Long id);
+
+	Livre getLivre(Long id);
+
+	List<Livre> getAllLivres();
+
+	Page<Livre> getAllLivresParPage(int page, int size);
+	
+	List<Livre> findByTitreLivre(String nom);
+	List<Livre> findByTitreLivreContains(String nom);
+	List<Livre> findByTitrePrix (String nom, Double prix);
+	List<Livre> findByGenre (Genre genre);
+	List<Livre> findByGenreIdGen(Long id);
+	List<Livre> findByOrderByTitreLivreAsc();
+	List<Livre> trierLivresTitresPrix();
+	
+}
+
+
+
+
